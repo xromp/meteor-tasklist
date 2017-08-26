ddcfd3cfd25460ae771a50124c26752299aa2334
rom <penaflor024@gmail.com>
2017-08-26 22:33:17 +0800

    init

---
+++ imports/api/task.js
@@ -0,0 +1,38 @@
+import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
+import { check } from 'meteor/check';
+ 
+export const Tasks = new Mongo.Collection('tasks');
+
+if (Meteor.isServer) {
+    Meteor.publish('tasks', function tasksPublication() {
+        return Tasks.find();
+    })
+}
+Meteor.methods({
+    'tasks.insert'(text){
+        check(text, String);
+
+        if(!Meteor.userId()){
+            throw new Meteor.Error('not-authorized');
+        }
+
+        Tasks.insert({
+            text,
+            createAt: new Date(),
+            owner:Meteor.userId(),
+            username:Meteor.user().username
+        })
+    },
+    'tasks.remove'(taskId){
+        check(taskId, String);
+
+        Tasks.remove(taskId);
+    },
+    'tasks.setChecked' (taskId, setChecked) {
+        check(taskId, String);
+        check(setChecked, Boolean);
+
+        Tasks.update(taskId, { $set : { checked : setChecked }});
+    }
+})
---
+++ imports/ui/task.js
@@ -0,0 +1,14 @@
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+import {Tasks} from '../api/task.js';
+
+import './task.html';
+
+Template.task.events({
+    'click .toggle-checked'(){
+        Meteor.call('tasks.setChecked', this._id, !this.checked);
+    },
+    'click .delete'(){
+        Meteor.call('tasks.remove',this._id);
+    }
+})
